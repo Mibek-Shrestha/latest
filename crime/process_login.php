21adf2756f41c33f22a312dd6797eda007f58155
Mibek Shrestha <79530597+Mibek-Shrestha@users.noreply.github.com>
2022-08-15 19:29:42 +0545

    Add files via upload

---
+++ crime/process_login.php
@@ -0,0 +1,27 @@
+<?php
+    session_start();
+    require_once "include/db_config.php";
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+            
+        $query = "select * from user where (username = '$username') and password = '$password'";
+
+        $res = mysqli_query($conn,$query);
+
+
+        $row = mysqli_fetch_array($res);
+            if($row["username"]=="admin")
+            {
+                $_SESSION["admin"] = $username;
+                header("location:admin/dash.php");
+            }
+            else
+            {
+                header("location:login.php?Error=invalid username or password");
+                // $error[] = 'incorrect credentials';
+            }
+
+
+}
